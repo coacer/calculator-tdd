5490698e0bec25fbd711992d13113874e9804c7f
coacer <coacer728h23@icloud.com>
2020-01-04 18:16:09 +0900

    Fix App componets

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App/App';
 import './index.css';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <h1>Hello</h1>,
+  <App />,
   document.getElementById('root')
 );
 
